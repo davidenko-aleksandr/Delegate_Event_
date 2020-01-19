c3d17499ccdb0a89d39b853240c59a0a087878a8
Aleksandr Davidenko <alex1190119022@gmail.com>
2020-01-19 23:32:45 +0200

    Add project files.

---
+++ Delegate_Event/Account.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegate_Event
+{
+    public delegate void AccountDelegate(string message);
+    class Account
+    {
+        //AccountDelegate _del;
+        int _sum;
+        public event AccountDelegate Added;
+        public event AccountDelegate Adding;
+        public event AccountDelegate Withraw;
+        //public void MethodToDelegate(AccountDelegate del)
+        //{
+        //    _del += del;
+        //}
+        //public void UnmethodToDelegate(AccountDelegate del)
+        //{
+        //    _del -= del; 
+        //}
+        public Account(int sum)
+        {
+            _sum = sum;
+        }
+        public void Put(int sum)
+        {
+            if (Adding != null) 
+                Adding($"На счёт добавляется: {sum}$");
+            _sum += sum;
+            if (Added!=null)
+            {
+                Added($"На счёт добавлено: {sum}$");
+            }
+            
+            
+        }
+        public void Withdraw(int sum)
+        {
+            if (_sum>=sum)
+            {
+                if (Adding != null) Adding($"Будет списано со счета: {sum}$");
+                    _sum -= sum;
+                if (Added != null)
+                {
+                    Added($"Списано со счета: {sum}$");
+                }
+                
+            }
+            else
+            {
+                if (Withraw != null)
+                {
+                    Withraw("На счёту недостаточно средств");
+                }
+                
+            }
+        }
+
+    }
+}
---
+++ Delegate_Event/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Delegate_Event
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(100);
+            //account.MethodToDelegate(Display);
+            //account.MethodToDelegate(ColorDisplay);
+            account.Adding += Display;
+            account.Added += ColorDisplay;
+            account.Put(50);
+            account.Withdraw(80);
+            //account.UnmethodToDelegate(ColorDisplay);
+            account.Withdraw(90);
+        }
+
+        static void Display(string message)
+        {
+            Console.WriteLine(message);
+        }
+        static void ColorDisplay(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
